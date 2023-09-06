e2f38e094390ac4d4727120572a56aef734aae23
Hester910 <hadassa.ester@ufrpe.br>
2023-09-06 17:44:44 -0300

    função solar_system_with_orbits

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 
+def Solar_System_with_orbits():
+    glDrawBuffer(GL_BACK)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    Solar_System()
+    Show_Orbits()
+
+
 def main():
     glutInit(sys.argv)
     glutInitContextVersion(1, 1)
